d857381429be27f2a6c81156b090a3bd9d0f8333
Lê Anh Minh <lamdev2104@gmail.com>
2025-10-18 13:47:29 +0700

    Create css-elearning.vinhuni.edu.vn-by-lamdev.user.js

---
+++ css-elearning.vinhuni.edu.vn-by-lamdev.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         CSS ELEARNING VINHUNI 
+// @description  CSS ELEARNING VINHUNI  By LAMDev
+// @require      https://raw.githubusercontent.com/leanhminh2104/Speep-Wed-By-LAMDev/refs/heads/main/css-elearning.vinhuni.edu.vn-by-lamdev-v2.js
+// @updateURL   https://raw.githubusercontent.com/leanhminh2104/Speep-Wed-By-LAMDev/refs/heads/main/css-elearning.vinhuni.edu.vn-by-lamdev-v2.js
+// @downloadURL https://raw.githubusercontent.com/leanhminh2104/Speep-Wed-By-LAMDev/refs/heads/main/css-elearning.vinhuni.edu.vn-by-lamdev-v2.js
+// @namespace    http://tampermonkey.net/
+// @version      2.0
+// @author       LAMDev
+// @match        *://*/*
+// @grant        none
+// @icon         https://elearning.vinhuni.edu.vn/pluginfile.php/1/theme_klass/logo/1736161506/logo.png
+// ==/UserScript==
