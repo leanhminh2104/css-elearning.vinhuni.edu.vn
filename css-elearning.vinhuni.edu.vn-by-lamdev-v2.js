f23f7d3fc50f37aa85080fec9011c5f387632fcc
Lê Anh Minh <lamdev2104@gmail.com>
2025-11-20 13:32:30 +0700

    Update css-elearning.vinhuni.edu.vn-by-lamdev-v2.js

--- css-elearning.vinhuni.edu.vn-by-lamdev-v2.js
+++ css-elearning.vinhuni.edu.vn-by-lamdev-v2.js
@@ -8,7 +8,7 @@
 // @license      MIT
 // @match        *://elearning.vinhuni.edu.vn/*
 // @match        *://congsv.vinhuni.edu.vn/*
-// @icon         https://www.google.com/s2/favicons?domain=elearning.vinhuni.edu.vn
+// @icon         https://elearning.vinhuni.edu.vn/pluginfile.php/1/theme_klass/logo/1736161506/logo.png
 // @homepageURL  https://elearning.vinhuni.edu.vn/
 // @supportURL   https://elearning.vinhuni.edu.vn/guide/
 // ==/UserScript==
