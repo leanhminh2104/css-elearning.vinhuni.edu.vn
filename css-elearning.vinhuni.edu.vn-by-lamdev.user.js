376dd24ba0eb6e1bb0ce4ed5ce0bc07341f0e0d5
Lê Anh Minh <lamdev2104@gmail.com>
2025-11-20 13:33:31 +0700

    Update user script metadata and URLs

--- css-elearning.vinhuni.edu.vn-by-lamdev.user.js
+++ css-elearning.vinhuni.edu.vn-by-lamdev.user.js
@@ -1,13 +1,14 @@
 // ==UserScript==
-// @name         CSS ELEARNING VINHUNI 
+// @name         CSS ELEARNING VINHUNI
 // @description  CSS ELEARNING VINHUNI  By LAMDev
-// @require      https://raw.githubusercontent.com/leanhminh2104/Speep-Wed-By-LAMDev/refs/heads/main/css-elearning.vinhuni.edu.vn-by-lamdev-v2.js
-// @updateURL   https://raw.githubusercontent.com/leanhminh2104/Speep-Wed-By-LAMDev/refs/heads/main/css-elearning.vinhuni.edu.vn-by-lamdev-v2.js
-// @downloadURL https://raw.githubusercontent.com/leanhminh2104/Speep-Wed-By-LAMDev/refs/heads/main/css-elearning.vinhuni.edu.vn-by-lamdev-v2.js
+// @require      https://raw.githubusercontent.com/leanhminh2104/css-elearning.vinhuni.edu.vn/refs/heads/main/css-elearning.vinhuni.edu.vn-by-lamdev-v2.js
+// @updateURL    https://raw.githubusercontent.com/leanhminh2104/css-elearning.vinhuni.edu.vn/refs/heads/main/css-elearning.vinhuni.edu.vn-by-lamdev-v2.js
+// @downloadURL  https://raw.githubusercontent.com/leanhminh2104/css-elearning.vinhuni.edu.vn/refs/heads/main/css-elearning.vinhuni.edu.vn-by-lamdev-v2.js
 // @namespace    http://tampermonkey.net/
 // @version      2.0
 // @author       LAMDev
-// @match        *://*/*
+// @match        *://elearning.vinhuni.edu.vn/*
+// @match        *://congsv.vinhuni.edu.vn/*
 // @grant        none
 // @icon         https://elearning.vinhuni.edu.vn/pluginfile.php/1/theme_klass/logo/1736161506/logo.png
 // ==/UserScript==
