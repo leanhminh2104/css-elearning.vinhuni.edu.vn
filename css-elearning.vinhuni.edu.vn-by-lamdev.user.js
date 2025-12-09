0d30172081a65301451c534b6c080fe3c1afb50a
Lê Anh Minh <lamdev2104@gmail.com>
2025-12-09 22:50:19 +0700

    Update user script metadata for version 3.0

--- css-elearning.vinhuni.edu.vn-by-lamdev.user.js
+++ css-elearning.vinhuni.edu.vn-by-lamdev.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
-// @name         CSS ELEARNING VINHUNI
-// @description  CSS ELEARNING VINHUNI  By LAMDev
-// @require      https://raw.githubusercontent.com/leanhminh2104/css-elearning.vinhuni.edu.vn/refs/heads/main/css-elearning.vinhuni.edu.vn-by-lamdev-v2.js
-// @updateURL    https://raw.githubusercontent.com/leanhminh2104/css-elearning.vinhuni.edu.vn/refs/heads/main/css-elearning.vinhuni.edu.vn-by-lamdev-v2.js
-// @downloadURL  https://raw.githubusercontent.com/leanhminh2104/css-elearning.vinhuni.edu.vn/refs/heads/main/css-elearning.vinhuni.edu.vn-by-lamdev-v2.js
+// @name         ELEARNING + CONGSV VINHUNI
+// @description  ELEARNING VINHUNI + CONGSV By LAM
+// @require      https://raw.githubusercontent.com/leanhminh2104/css-elearning.vinhuni.edu.vn/refs/heads/main/css-elearning.vinhuni.edu.vn-by-lamdev-v3.js
+// @updateURL    https://raw.githubusercontent.com/leanhminh2104/css-elearning.vinhuni.edu.vn/refs/heads/main/css-elearning.vinhuni.edu.vn-by-lamdev-v3.js
+// @downloadURL  https://raw.githubusercontent.com/leanhminh2104/css-elearning.vinhuni.edu.vn/refs/heads/main/css-elearning.vinhuni.edu.vn-by-lamdev-v3.js
 // @namespace    http://tampermonkey.net/
-// @version      2.0
-// @author       LAMDev
+// @version      3.0
+// @author       LAM - DICHVUSALE.IO.VN - LEANHMINH.IO.VN
 // @match        *://elearning.vinhuni.edu.vn/*
 // @match        *://congsv.vinhuni.edu.vn/*
 // @grant        none
